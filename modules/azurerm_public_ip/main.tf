77b7e7a3eb77d3314a26e90789021c2ef0b23091
Mimansha Dutta <mimanshaedu@gmail.com>
2025-06-30 12:56:05 +0530

    added comments in pip

--- modules/azurerm_public_ip/main.tf
+++ modules/azurerm_public_ip/main.tf
@@ -1,3 +1,10 @@
+# This module creates an Azure Public IP resource.
+# It requires the following variables:
+# - public_ip_name: The name of the public IP resource.
+# - rg_name: The name of the resource group where the public IP will be created.
+# - location: The Azure region where the public IP will be created.
+# - allocation_method: The allocation method for the public IP (e.g., Static or Dynamic
+
 resource "azurerm_public_ip" "pip" {
   name                = var.public_ip_name
   resource_group_name = var.rg_name
